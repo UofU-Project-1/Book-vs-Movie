444e208fd8c099c0dad7ab417184b7fd4fd45226
corbinjarms <corbin@corbinjarms.com>
2020-09-01 20:16:02 -0700

    saving rating into variable

--- assets/movieratings.js
+++ assets/movieratings.js
@@ -2,7 +2,7 @@ console.log("This script is working")
 document.getElementById("search-btn").addEventListener("click", getMovieTitle);
 
 imdbID = ''
-
+movieRating = ''
 function getMovieTitle() {
     let search = document.getElementById("search-movie-title").value
     console.log("This is what was searched " + search)
@@ -32,7 +32,8 @@ function searchIMDB(imdbEndpoint) {
             console.log(response)
             document.getElementById("movie-director").textContent= "Director: " + response.Director
             document.getElementById("movie-year").textContent= "Release Date: " + response.Year
-            document.getElementById("movie-rating").textContent= "Rating: " + response.imdbRating
+            document.getElementById("movie-rating").textContent= "Rating: " + response.imdbRating * 10
+            let movieRating = response.imdbRating * 10
             //document.getElementById("movie-title").textContent= response.Search[0].Title;
             //let imdbID = document.getElementById("movie-director").textContent= response.Search[0].imdbID;
             
